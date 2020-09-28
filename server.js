5f0bc2b61cdd1702e835605d60db43a715418757
Ayush Bharadwaj <ABharadwaj@dealersocket.com>
2020-09-28 23:59:54 +0530

    fetch changes

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ var express = require('express');
 var app = express();    
 const fetch = require('node-fetch');              
 app.get('/', (req, res) => {
-    fetch(`https://www.cricbuzz.com/api/cricket-match/commentary/30370`)
-        .then(res => res.json())
-        .then(json => {
-            return res.send(json);
-    });
+  fetch('https://www.cricbuzz.com/api/cricket-match/commentary/30370')
+  .then(response => response.json())
+  .then(data => {
+    // console.log('Success:', data);
+    return res.send(data);
+  })
+  .catch((error) => {
+    // console.error('Error:', error);
+    return res.send(`400 error`);
+  });
+
 });
 var port = process.env.PORT || 3030;  
 app.listen(port, () =>
