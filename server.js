af46dc94be9d88a4a83ff9be1808b885ae271e92
Ayush Bharadwaj <ABharadwaj@dealersocket.com>
2020-10-02 18:24:25 +0530

    first commit update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  fetch('https://sdata.ndtv.com/sportz/cricket/xml/kpmi10012020197700.json?t=11601559592644')
+  fetch('https://sdata.ndtv.com/sportz/cricket/xml/cksh10022020197701.json?_=1601643186323')
   .then(response => response.json())
   .then(data => {
     console.log('Success:', data);
