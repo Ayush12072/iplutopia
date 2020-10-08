92b8616570523c36c2bcf2bfe9027e7059f753bb
Ayush Bhardwaj <Ayush12072@users.noreply.github.com>
2020-10-08 16:35:03 +0530

    new Modification

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-var express = require('express');       
-var app = express();    
-const fetch = require('node-fetch');      
+var express = require('express');
+var app = express();
+const fetch = require('node-fetch');
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
 });
 
 app.get('/', (req, res) => {
-  fetch('https://sdata.ndtv.com/sportz/cricket/xml/bcdd10052020197706.json?t=51601905143301')
-  .then(response => response.json())
-  .then(data => {
-    console.log('Success:', data);
-    return res.send(data);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-    return res.send(`400 error`);
-  });
+    fetch('https://sdata.ndtv.com/sportz/cricket/xml/krck10072020197708.json?t=71602081932303')
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            return res.send(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            return res.send(`400 error`);
+        });
 
 });
-var port = process.env.PORT || 3030;  
+var port = process.env.PORT || 3030;
 app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`),
+    console.log(`Example app listening on port ${port}!`),
 );
